dc0e2f05dfb37b3e59621d2a259f6db89c97c1c0
Oleksii Novikov <oleksii@novikraft.com>
2023-01-05 22:00:58 +0200

    feat: build the tree of islands

--- src/island.ts
+++ src/island.ts
@@ -1,17 +1,21 @@
 export interface VoyageIsland {
   el: HTMLElement;
+  children: VoyageIsland[];
 }
 
 export interface VoyageIslandProps {
   el: HTMLElement;
+  children: VoyageIsland[];
 }
 
 export type VoyageIslandFactory = (props: VoyageIslandProps) => VoyageIsland;
 
 export class Island implements VoyageIsland {
+  children: VoyageIsland[];
   el: HTMLElement;
 
-  constructor({ el }: VoyageIslandProps) {
+  constructor({ children, el }: VoyageIslandProps) {
+    this.children = children;
     this.el = el;
   }
 
--- src/lighthouse.ts
+++ src/lighthouse.ts
@@ -2,25 +2,25 @@ import { Island, VoyageIsland, VoyageIslandFactory } from "./island";
 
 export interface VoyageLighthouse {
   createIsland: VoyageIslandFactory;
-  discovery(el: unknown): VoyageIsland[];
+  discovery(el: HTMLElement): VoyageIsland;
 }
 
 export interface VoyageLighthouseProps {
-  createIsland: VoyageIslandFactory;
+  createIsland?: VoyageIslandFactory;
 }
 
 export class Lighthouse implements VoyageLighthouse {
   createIsland: VoyageIslandFactory;
 
-  constructor({ createIsland = Island.of }: VoyageLighthouseProps) {
+  constructor({ createIsland = Island.of }: VoyageLighthouseProps = { }) {
     this.createIsland = createIsland;
   }
 
-  discovery(el: unknown): VoyageIsland[] {
-    const isEl = el instanceof HTMLElement;
-    if (!isEl) throw new Error('`el` is not an instance of HTMLElement.');
-    return Array.from(el.querySelectorAll('[data-voyage-island]'))
-      .filter((el: Element): el is HTMLElement => el instanceof HTMLElement)
-      .map(el => this.createIsland({ el }));
+  discovery(el: HTMLElement): VoyageIsland {
+    const children = Array.from(el.querySelectorAll('[data-island]'))
+      .filter((childEl): childEl is HTMLElement => childEl instanceof HTMLElement)
+      .filter(childEl => childEl.parentElement?.closest('[data-island]') === el)
+      .map(childEl => this.discovery(childEl));
+    return this.createIsland({ el, children });
   }
 }
