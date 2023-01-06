9a75fcf60a45e9f664dee526adb2782b6645a943
Oleksii Novikov <oleksii@novikraft.com>
2023-01-06 22:35:01 +0200

    feat: the discovery() accepts unkown element

--- src/lighthouse.ts
+++ src/lighthouse.ts
@@ -2,7 +2,7 @@ import { Island, VoyageIsland, VoyageIslandFactory } from "./island";
 
 export interface VoyageLighthouse {
   createIsland: VoyageIslandFactory;
-  discovery(el: HTMLElement): VoyageIsland;
+  discovery(el: unknown): VoyageIsland;
 }
 
 export interface VoyageLighthouseProps {
@@ -16,11 +16,17 @@ export class Lighthouse implements VoyageLighthouse {
     this.createIsland = createIsland;
   }
 
-  discovery(el: HTMLElement): VoyageIsland {
+  discovery(el: unknown): VoyageIsland {
+    this.validateIsland(el);
     const children = Array.from(el.querySelectorAll('[data-island]'))
       .filter((childEl): childEl is HTMLElement => childEl instanceof HTMLElement)
       .filter(childEl => childEl.parentElement?.closest('[data-island]') === el)
       .map(childEl => this.discovery(childEl));
     return this.createIsland({ el, children });
+  }
+
+  private validateIsland(el: unknown): asserts el is HTMLElement {
+    const isEl = el && el instanceof HTMLElement;
+    if (!isEl) throw new TypeError('the `el` is not an instance of HTMLElement');
   }
 }
