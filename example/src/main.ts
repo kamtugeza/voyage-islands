d936ac59ca0aab0926d72696cec31afea7e67cb7
Oleksii Novikov <oleksii@novikraft.com>
2023-01-09 20:34:32 +0200

    Revert "ref: make flexible the island key/selector"
    This reverts commit dc35f1136136259ecda03acef0b3363b3263d47d.

--- example/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { Voyager } from '../../lib/index';
-
-
-document.addEventListener('DOMContentLoaded', async () => {
-  const voyager = new Voyager();
-  console.dir(voyager);
-  const dom = await Voyager.of().discovery(document.querySelector('body'));
-  console.dir(dom);
-});
